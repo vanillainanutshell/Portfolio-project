151715f2ff94d61b19ba507349f4f5813a4580c6
Vanillainanutshell <maurovasconcelos11@gmail.com>
2021-12-14 17:08:19 -0200

    Add more javascript elements and new icons on imgs folder

--- scripts.js
+++ scripts.js
@@ -4,39 +4,6 @@ const span = document.getElementsByClassName('close')[0];
 const close1 = document.getElementById('close');
 const close2 = document.getElementById('close2');
 const close3 = document.getElementById('close3');
-let popup = document.createElement('div');
-let popupTitle = document.createElement('h2');
-let list = document.createElement('ul');
-let listContent = document.createElement('li');
-
-popup.id = 'popupModal';
-popup.className = 'popupModal';
-
-popupTitle.id = 'ptitle';
-popupTitle.textContent = 'Keeping track of hundreds of components website';
-popupTitle.className = 'ptitle';
-
-list.id = 'popupList';
-
-listContent = document.createElement('li');
-
-listContent = document.createElement('li');
-
-listContent = document.createElement('li');
-
-listContent = document.createElement('li');
-
-listContent = document.createElement('li');
-listContent.textContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
-
-listContent = document.createElement('li');
-
-
-popup.appendChild('popupTitle');
-popup.appendChild('list');
-list.appendChild('listContent');
-
-document.getElementById.appendChild('mobileModal');
 
 btn.onclick = function() {
     modal.style.display = 'block';
@@ -63,3 +30,70 @@ window.onclick = function(event) {
         modal.style.display = 'none';
     }
 };
+
+let popup = document.createElement('div');
+popup.id = 'popupModal';
+popup.className = 'popupModal';
+popup.appendChild('popupTitle');
+popup.appendChild('list');
+
+let popupTitle = document.createElement('h2');
+popupTitle.id = 'ptitle';
+popupTitle.textContent = 'Keeping track of hundreds of components website';
+popupTitle.className = 'ptitle';
+
+const popupImg = document.createElement('img');
+popupImg.setAttribute('src', 'imgs/snapshoot.svg');
+popupImg.className = 'popupImg';
+
+const icon1 = document.createElement('img');
+icon1.setAttribute('src', 'seelive.svg');
+icon1.className = 'icon1';
+
+const icon2 = document.createElement('img');
+icon2.setAttribute('src', 'imgs/popupcat.svg');
+
+let listContent = document.createElement('li');
+listContent = document.createElement('li');
+listContent.textContent = "html";
+listContent.className = "whitebuttons";
+
+listContent = document.createElement('li');
+listContent.textContent = "Bootstrap";
+listContent.className = "whitebuttons";
+
+listContent = document.createElement('li');
+listContent.textContent = "Ruby on rails";
+listContent.className = "whitebuttons";
+
+listContent = document.createElement('li');
+listContent.appendChild('popupImg');
+
+listContent = document.createElemengt('li');
+listContent.textContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
+
+listContent = document.createElement('li');
+listContent.textContent = "See Live";
+listContent.className = 'flatbutton';
+listContent.appendChild('icon1');
+
+listContent = document.createElement('li');
+listContent.textContent = "See Source";
+listContent.className = 'flatbutton';
+listContent.appendChild('icon2');
+
+
+let list = document.createElement('ul');
+list.id = 'popupList';
+list.appendChild('listContent');
+
+document.getElementById.appendChild('mobileModal');
+
+const closeButton = document.createElement('button');
+closeButton.className = 'close';
+list.appendChild('closeButton');
+
+const btnPopup = document.querySelectorAll('.btnpopup');
+btnPopup.onclick = function() {
+    popup.style.display = 'block';
+}
