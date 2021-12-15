5cf14dd7914e562e7b1681a0f1df2bd336562734
Vanillainanutshell <maurovasconcelos11@gmail.com>
2021-12-15 15:45:19 -0200

    Fix semi-transparent background behing popup screen

--- scriptpopup.js
+++ scriptpopup.js
@@ -6,13 +6,13 @@ const info = {
     live: 'https://vanillainanutshell.github.io/Portfolio-project/',
     source: 'https://github.com/vanillainanutshell/Portfolio-project',
 }
-let transparente = document.createElement('div');
+
 let popup = document.createElement('div');
 const body = document.getElementById('body');
-transparente.appendChild(popup);
-popup.className = 'popup';
+popup.className = 'containertransp';
 popup.id = 'popup';
 popup.innerHTML = `
+    <div class = 'popup'>
     <h2 class="popuptitle">${info.title[1]}</h2>
     <span class= "close">&times;</span>
     <ul class="technologies">
@@ -26,44 +26,45 @@ popup.innerHTML = `
     <li class="flatbutton"><a href="${info.live}">See live   <img src="imgs/seelive.svg"></a></li>
     <li class="flatbutton"><a href="${info.live}">See source </a> <img src="imgs/popupcat.svg"></li>
     </ul>
+    </div>
 `
-body.appendChild(transparente);
+body.appendChild(popup);
 
 
 const popupbutton1 = document.getElementById('popupbtn1');
 popupbutton1.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton2 = document.getElementById('popupbtn2');
 popupbutton2.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton3 = document.getElementById('popupbtn3');
 popupbutton3.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton4 = document.getElementById('popupbtn4');
 popupbutton4.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton5 = document.getElementById('popupbtn5');
 popupbutton5.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton6 = document.getElementById('popupbtn6');
 popupbutton6.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton7 = document.getElementById('popupbtn7');
 popupbutton7.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 const popupbutton8 = document.getElementById('popupbtn8');
 popupbutton8.addEventListener('click', function() {
-    transparente.style.display = 'block';
+    popup.style.display = 'block';
 });
 window.onclick = function(event) {
     if (event.target === popup) {
-        transparente.style.display = 'none';
+        popup.style.display = 'none';
     }
 };
