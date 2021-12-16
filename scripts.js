14f642bd605d7dce706608b3d950a2a8da2c3075
David Vergaray <david.vergaray.a@gmail.com>
2021-12-15 21:33:11 -0300

    Finished Javascript coding

--- scripts.js
+++ scripts.js
@@ -1,94 +1,132 @@
-const modal = document.getElementById('mobileModal');
-const btn = document.getElementById('burgir');
-const span = document.getElementsByClassName('close')[0];
-
-const close1 = document.getElementById('close');
-const close2 = document.getElementById('close2');
-const close3 = document.getElementById('close3');
+const info = {
+	title: 'Multi Post Stories',
+	text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+	image: 'imgs/snapshoot.svg',
+	technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+	live: 'https://vanillainanutshell.github.io/Portfolio-project/',
+	source: 'https://github.com/vanillainanutshell/Portfolio-project',
+}
 
 let popup = document.createElement('div');
-let popupTitle = document.createElement('h2');
+const body = document.getElementById('body');
+popup.className = 'containertransp';
+popup.id = 'popup';
+popup.innerHTML = `
+	<div class ='popup'>
+	<h2 class="popuptitle">${info.title}</h2>
+	<span id="closer" class= "close">&times;</span>
+	<ul class="technologies">
+	<li class="whitebuttons">${info.technologies[0]}</li>
+	<li class="whitebuttons">${info.technologies[1]}</li>
+	<li class="whitebuttons">${info.technologies[2]}</li>
+	</ul>
+	<img class="popimg" alt="project" src= "${info.image}">
+	<p>${info.text}</p>
+	<ul class="links">
+	<li class="flatbutton"><a href="${info.live}">See live   <img src="imgs/seelive.svg"></a></li>
+	<li class="flatbutton"><a href="${info.live}">See source </a> <img src="imgs/popupcat.svg"></li>
+	</ul>
+	</div>
+`
+body.appendChild(popup);
+const info2 = {
+	title: 'Keeping track of hundreds  of components website',
+	text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.',
+	image: 'imgs/snapshoot.svg',
+	technologies: ['html', 'Bootstrap', 'Ruby on rails'],
+	live: 'https://vanillainanutshell.github.io/Portfolio-project/',
+	source: 'https://github.com/vanillainanutshell/Portfolio-project',
+}
 
-let list = document.createElement('ul');
-let listContent = document.createElement('li');
+let popupD = document.createElement('div');
+popupD.className = 'containertransp';
+popupD.id = 'popupD';
+popupD.innerHTML = `
+	<div class ='popup'>
+	<h2 class="popuptitle">${info2.title}</h2>
+	<span id="closer2" class= "close">&times;</span>
+	<ul class="technologies">
+	<li class="whitebuttons">${info2.technologies[0]}</li>
+	<li class="whitebuttons">${info2.technologies[1]}</li>
+	<li class="whitebuttons">${info2.technologies[2]}</li>
+	</ul>
+	<div class="flextonator">
+	<img class="popimg" alt="project" src= "${info2.image}">
+	<div  class="flextonatorreturns">
+	<p>${info2.text}</p>
+	<ul class="links">
+	<li class="flatbutton"><a href="${info2.live}">See live   <img src="imgs/seelive.svg"></a></li>
+	<li class="flatbutton"><a href="${info2.live}">See source </a> <img src="imgs/popupcat.svg"></li>
+	</ul>
+	</div>
+	</div>
+	</div>
+`
+body.appendChild(popupD);
 
-const popupImg = document.createElement('img');
-const icon1 = document.createElement('img');
-const icon2 = document.createElement('img');
+const popupbutton1 = document.getElementById('popupbtn1');
+popupbutton1.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-popupImg.setAttribute('src', 'imgs/snapshoot.svg');
-popupImg.className = 'popupImg';
+const popupbutton2 = document.getElementById('popupbtn2');
+popupbutton2.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-icon1.setAttribute('src', 'seelive.svg');
-icon1.className = 'icon1';
-icon2.setAttribute('src', 'imgs/popupcat.svg');
+const popupbutton3 = document.getElementById('popupbtn3');
+popupbutton3.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-popup.id = 'popupModal';
-popup.className = 'popupModal';
+const popupbutton4 = document.getElementById('popupbtn4');
+popupbutton4.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-popupTitle.id = 'ptitle';
-popupTitle.textContent = 'Keeping track of hundreds of components website';
-popupTitle.className = 'ptitle';
+const popupbutton5 = document.getElementById('popupbtn5');
+popupbutton5.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-list.id = 'popupList';
+const popupbutton6 = document.getElementById('popupbtn6');
+popupbutton6.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-listContent = document.createElement('li');
-listContent.textContent = "html";
-listContent.className = "whitebuttons";
+const popupbutton7 = document.getElementById('popupbtn7');
+popupbutton7.addEventListener('click', function() {
+	popup.style.display = 'block';
+});
 
-listContent = document.createElement('li');
-listContent.textContent = "Bootstrap";
-listContent.className = "whitebuttons";
+const popupbutton8 = document.getElementById('popupbtn8');
+popupbutton8.addEventListener('click', function() {
+	popupD.style.display = 'block';
+});
 
-listContent = document.createElement('li');
-listContent.textContent = "Ruby on rails";
-listContent.className = "whitebuttons";
+const popupbutton9 = document.getElementById('popupbtn9');
+popupbutton9.addEventListener('click', function() {
+	popupD.style.display = 'block';
+});
 
-listContent = document.createElement('li');
-listContent.appendChild('popupImg');
+const span2 = document.getElementById('closer');
+span2.addEventListener('click', function() {
+	popup.style.display = 'none';
+});
 
-listContent = document.createElement('li');
-listContent.textContent = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releax map lapora verita.'
+const span3 = document.getElementById('closer2');
+span3.addEventListener('click', function() {
+	popupD.style.display = 'none';
+});
 
-listContent = document.createElement('li');
-listContent.textContent = "See Live";
-listContent.className = 'flatbutton';
-listContent.appendChild('icon1');
-
-listContent = document.createElement('li');
-listContent.textContent = "See Source";
-listContent.className = 'flatbutton';
-listContent.appendChild('icon2');
-
-popup.appendChild('popupTitle');
-popup.appendChild('list');
-
-list.appendChild('listContent');
-
-document.getElementById.appendChild('mobileModal');
-
-btn.onclick = function() {
-	modal.style.display = 'block';
-};
-
-span.onclick = function() {
-	modal.style.display = 'none';
-};
-
-close1.onclick = function() {
-	modal.style.display = 'none';
-};
-
-close2.onclick = function() {
-	modal.style.display = 'none';
-};
-
-close3.onclick = function() {
-	modal.style.display = 'none';
+window.onclick = function(event) {
+	if (event.target === popup) {
+			popup.style.display = 'none';
+	}
 };
 
 window.onclick = function(event) {
-	if (event.target === modal) {
-		modal.style.display = 'none';
+	if (event.target === popupD) {
+			popupD.style.display = 'none';
 	}
 };
