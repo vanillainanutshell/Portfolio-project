7e0553b7e5d46509a30ddf3088cc10f49d8ac759
Vanillainanutshell <maurovasconcelos11@gmail.com>
2021-12-16 17:44:58 -0200

    Fix linters

--- scripts.js
+++ scripts.js
@@ -5,57 +5,60 @@ const close1 = document.getElementById('close');
 const close2 = document.getElementById('close2');
 const close3 = document.getElementById('close3');
 
-btn.onclick = function() {
-    modal.style.display = 'block';
+btn.onclick = function () {
+  modal.style.display = 'block';
 };
 
-span.onclick = function() {
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+close1.onclick = function () {
+  modal.style.display = 'none';
+};
+
+close2.onclick = function () {
+  modal.style.display = 'none';
+};
+
+close3.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
     modal.style.display = 'none';
-};
-
-close1.onclick = function() {
-    modal.style.display = 'none';
-};
-
-close2.onclick = function() {
-    modal.style.display = 'none';
-};
-
-close3.onclick = function() {
-    modal.style.display = 'none';
-};
-
-window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = 'none';
-    }
+  }
 };
 
 function upperCaseErrorMessage() {
-    const messageContainer = document.getElementById('errorMessage');
-    messageContainer.innerHTML = '<b>Error</b>: Email should only contain lower cases.';
+  const messageContainer = document.getElementById('errorMessage');
+  messageContainer.innerHTML = '<b>Error</b>: Email should only contain lower cases.';
 }
 
 function checkEmailUppercase() {
-    let email = document.getElementById('email').value;
-    email = email.split('');
+  let email = document.getElementById('email').value;
+  email = email.split('');
 
-    for (let i = 0; i < email.length; i += 1) {
-        if (email[i].match(/[a-z]/i)) {
-            if (email[i] === email[i].toUpperCase()) {
-                const errorContainer = document.getElementById('errorMessage');
-                errorContainer.style.display = 'block';
-                upperCaseErrorMessage();
+  for (let i = 0; i < email.length; i += 1) {
+    if (email[i].match(/[a-z]/i)) {
+      if (email[i] === email[i].toUpperCase()) {
+        const errorContainer = document.getElementById('errorMessage');
+        errorContainer.style.display = 'block';
+        upperCaseErrorMessage();
 
-                errorContainer.onclick = function() {
-                    $("#errorMessage").fadeOut(5000);
-                }
-                return false;
-            }
-        }
+        return false;
+      }
     }
-    return true;
+  }
+  return true;
 }
-document.getElementById('myForm').onsubmit = function() {
-    return checkEmailUppercase();
+const errorContainer = document.getElementById('errorMessage');
+const submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+  errorContainer.fadeOut(3000);
+});
+
+document.getElementById('myForm').onsubmit = function () {
+  return checkEmailUppercase();
 };
